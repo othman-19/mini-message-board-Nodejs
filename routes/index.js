bbb218b030613d3feda476ce62be276081a1453b
othmane <othman-19@hotmail.fr>
2020-09-12 09:52:33 +0100

    set the messages index router and display them in the index view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// eslint-disable-next-line no-unused-vars
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Messages Board', messages: messages });
 });
 
 module.exports = router;
